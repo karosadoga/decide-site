009ee533496f707316a80506922a133ee6552305
karosadoga <karosadoga@gmail.com>
2026-04-27 20:23:24 -0500

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,9 @@
-import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
-
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
 
 export default config
